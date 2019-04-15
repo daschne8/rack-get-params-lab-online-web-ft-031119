5196cc432ac5c82e93b67c2f84a35598a0e6e6c3
Daniel Schneider <daschne8@gmail.com>
2019-04-15 22:31:27 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -21,8 +21,8 @@ class Application
         resp.write "Your cart is empty"
       end
     elsif req.path.match(/add/)
-      item = search_term = req.params["q"]
-      # item = handle_search(search_term)
+      search_term = req.params["item"]
+      item = handle_search(search_term)
       resp.write "#{item}\n"
       if @@items.include?(item)
         @@cart << item
